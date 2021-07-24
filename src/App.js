4f0440b9309af6f227605b74bf6578d0ab174132
Prabodh Meshram <prabodh.meshram7@gmail.com>
2021-07-25 01:12:06 +0530

    Some refactoring and init code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import { LoginPage } from './pages/LoginPage';
+import { HomePage } from './pages/HomePage';
+import PrivateRoute  from './components/privateRoute';
+import Logout  from './components/logout';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+          <Switch>
+            <PrivateRoute path="/home" component={HomePage} />
+            <Route path="/logout">
+              <Logout />
+            </Route>
+            <Route path="/">
+              <LoginPage />
+            </Route>
+          </Switch>
+      </div>
+    </Router>
   );
 }
 
